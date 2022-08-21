e6afe02928df68c64048b85b00ec9be0e12ae239
ivelinamisova <ivelina_misova@abv.bg>
2022-08-22 00:07:22 +0300

    init

--- pay.py
+++ pay.py
@@ -1,9 +1,13 @@
-hours = float(input('Enter Hours: '))
-rate = float(input('Enter Rate: '))
+try:
+    hours = float(input('Enter Hours: '))
+    rate = float(input('Enter Rate: '))
 
-if hours <= 40:
-    pay = hours * rate
-else:
-    pay = ((hours - 40) * (1.5 * rate)) + (40 * rate)
+    if hours <= 40:
+        pay = hours * rate
+    else:
+        pay = ((hours - 40) * (1.5 * rate)) + (40 * rate)
+        print('Pay =', pay)
 
-print('Pay =', pay)
+except Exception as e:
+    print("Error has occurred", e)
+
