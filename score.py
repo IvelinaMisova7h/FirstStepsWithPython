e024baf04b21a433151dc3fc2312ef85f45c86bc
ivelinamisova <ivelina_misova@abv.bg>
2022-08-23 23:39:42 +0300

    init

--- score.py
+++ score.py
@@ -1,2 +1,16 @@
-score = float('Input score: ')
-
+try:
+    score = float(input('Input score: '))
+    if 0.9 <= score <= 1.0:
+        print("A")
+    elif 0.8 <= score <= 0.9:
+        print("B")
+    elif 0.7 <= score <= 0.8:
+        print("C")
+    elif 0.6 <= score <= 0.7:
+        print("D")
+    elif 0 < score <= 0.6:
+        print("F")
+    else:
+        print("Please input score between 0.1 and 1.0!")
+except:
+    print("Error!")
