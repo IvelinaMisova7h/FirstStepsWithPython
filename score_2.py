761def2d6a0a9cf0b2446c271e6e5add46f42cca
ivelinamisova <ivelina_misova@abv.bg>
2022-08-26 14:37:40 +0300

    init

--- score_2.py
+++ score_2.py
@@ -1,33 +1,22 @@
 # Here is another solution for score program, using function.
-try:
-    s = float(input('Enter score: '))
+
+s = float(input('Enter score: '))
 
 
-    def compute_grade(score):
-        if 0.9 <= score <= 1.0:
-            print("A")
-        elif 0.8 <= score <= 0.9:
-            print("B")
-        elif 0.7 <= score <= 0.8:
-            print("C")
-        elif 0.6 <= score <= 0.7:
-            print("D")
-        elif 0 < score <= 0.6:
-            print("F")
-        else:
-            print("Please input score between 0.1 and 1.0!")
-    compute_grade(s)
-
-except:
-    if  == 'perfect':
-        print('Bad score! ')
+def compute_grade(score):
+    if 0.9 <= score <= 1.0:
+        print("A")
+    elif 0.8 <= score <= 0.9:
+        print("B")
+    elif 0.7 <= score <= 0.8:
+        print("C")
+    elif 0.6 <= score <= 0.7:
+        print("D")
+    elif 0 < score <= 0.6:
+        print("F")
     else:
-        print('Error! ')
+        if not score <= 1.0 or score < 0.0:
+            raise AssertionError('Scores must be within the 1.0 and 0.0 range!')
 
 
-
-
-
-
-
-
+compute_grade(s)
