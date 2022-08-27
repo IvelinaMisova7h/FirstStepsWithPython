a63d18aacc475ff677afb96013c4dbc0365bd17e
ivelinamisova <ivelina_misova@abv.bg>
2022-08-27 16:00:50 +0300

    init

--- working_whit_list.py
+++ working_whit_list.py
@@ -1,13 +1,27 @@
-total = 0
-count = 0
+# total = 0
+# count = 0
+
+# while True:
+    # n = input('Enter a number: ')
+    # if n == 'done':
+        # break
+    # value = float(n)
+    # total += value
+    # count += 1
+
+# average = total / count
+# print('Average: ', average)
+
+# Another solution with creating an empty list.
+
+num_list = list()
 
 while True:
     n = input('Enter a number: ')
     if n == 'done':
         break
     value = float(n)
-    total += value
-    count += 1
+    num_list.append(value)
 
-average = total / count
+average = sum(num_list) / len(num_list)
 print('Average: ', average)
