c1e06d8a0b8d4aaae3e346dd01377af24cb267ce
ivelinamisova <ivelina_misova@abv.bg>
2022-08-28 16:43:46 +0300

    init

---
+++ read_a_file/from_count.py
@@ -0,0 +1,9 @@
+file_name = input('Enter a file name: ')
+file_handle = open(file_name)
+
+for line in file_handle:
+    t = line.rstrip()
+    word = line.split()
+    if len(word) < 3 or word[0] != 'From':  # Guardian in a compound statement.
+        continue
+    print(word[1])
