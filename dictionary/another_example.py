694ebf94af8dd2892cf6053898bc26143f4f10d5
ivelinamisova <ivelina_misova@abv.bg>
2022-08-30 19:10:20 +0300

    init

--- dictionary/another_example.py
+++ dictionary/another_example.py
@@ -1,6 +1,8 @@
 file_name = input('Enter a file name: ')
-if len(file_name) < 1 : file_name = 'clown.txt'
+if len(file_name) < 1: file_name = 'clown.txt'
 file_handle = open(file_name)
+
+di = dict()
 
 for line in file_handle:
     line = line.rstrip()
@@ -8,4 +10,25 @@ for line in file_handle:
     words = line.split()
     # print(words)
     for word in words:
-        print(word)
+
+# second solution
+
+        # print('**', word, di.get(word, -99))
+        old_count = di.get(word, 0)
+        print(word, 'old', old_count)
+        new_count = old_count + 1
+        di[word] = new_count
+        print(word, 'new', new_count)
+
+# first solution
+        # print(word)
+        # if word in di:
+        #     di[word] += 1
+        #     print('Existing')
+        # else:
+        #     di[word] = 1
+        #     print('**NEW**')
+        # print(di[word])
+
+
+print(di)
