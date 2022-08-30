8cb450b66a9fdc523bf309a4455e9897f7ce8745
ivelinamisova <ivelina_misova@abv.bg>
2022-08-30 19:32:48 +0300

    init

--- dictionary/another_example.py
+++ dictionary/another_example.py
@@ -1,5 +1,6 @@
 file_name = input('Enter a file name: ')
-if len(file_name) < 1: file_name = 'clown.txt'
+if len(file_name) < 1:
+    file_name = 'clown.txt'
 file_handle = open(file_name)
 
 di = dict()
@@ -10,25 +11,40 @@ for line in file_handle:
     words = line.split()
     # print(words)
     for word in words:
+        #
+        # # Second solution
+        #
+        #         # print('**', word, di.get(word, -99))
+        #         old_count = di.get(word, 0)    # If the key is not there the count is zero
+        #         print(word, 'old', old_count)
+        #         new_count = old_count + 1
+        #         di[word] = new_count
+        #         print(word, 'new', new_count)
+        #
+        #         # The second solution in two lines. Idiom: retrieve/create/update counter
+        di[word] = di.get(word, 0) + 1
+#         print(word, 'new', di[word])
+#
+# # First solution
+#         # print(word)
+#         # if word in di:
+#         #     di[word] += 1
+#         #     print('Existing')
+#         # else:
+#         #     di[word] = 1
+#         #     print('**NEW**')
+#         # print(di[word])
+#
+#
+# #print(di)
+#
 
-# second solution
-
-        # print('**', word, di.get(word, -99))
-        old_count = di.get(word, 0)
-        print(word, 'old', old_count)
-        new_count = old_count + 1
-        di[word] = new_count
-        print(word, 'new', new_count)
-
-# first solution
-        # print(word)
-        # if word in di:
-        #     di[word] += 1
-        #     print('Existing')
-        # else:
-        #     di[word] = 1
-        #     print('**NEW**')
-        # print(di[word])
-
-
-print(di)
+# Here we find the most common word.
+largest = -1
+the_word = None
+for k, v in di.items():
+    print(k, v)
+    if v > largest:
+        largest = v
+        the_word = k  # Capture/remember the key that was largest.
+print('Done', the_word, largest)
