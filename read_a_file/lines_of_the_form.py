8b272fd92e21e69d6e8fb7510cb985ea17bd6687
ivelinamisova <ivelina_misova@abv.bg>
2022-09-02 17:40:22 +0300

    init

--- read_a_file/lines_of_the_form.py
+++ read_a_file/lines_of_the_form.py
@@ -2,8 +2,13 @@ import re
 # file_name = input('Enter a file name: ')
 file_handle = open('mbox-short.txt')
 
+lst = list()
 for line in file_handle:
     line = line.rstrip()
-    x = re.findall('\\d', line)
-    if len(x) > 0:
-        print(x)
+    num = re.findall('^New Revision: ([0-9.]+)', line)
+    if len(num) > 0:
+        for number in num:
+            number = float(number)
+        lst.append(number)
+avg = sum(lst) / len(lst)
+print(avg)
